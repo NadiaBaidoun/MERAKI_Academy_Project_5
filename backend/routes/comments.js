d45d01c1b6ff05ca4aaeccc21afe635cb7367d2f
NadiaBaidoun <nadia.w.baidoun@gmail.com>
2022-06-01 14:21:31 +0300

    create Routers

---
+++ backend/routes/comments.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+
+
+const commentsRouter = express.Router();
+
+
+module.exports = commentsRouter;
---
+++ backend/routes/likes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+
+
+const likeRouter = express.Router();
+
+
+
+module.exports = likeRouter;
