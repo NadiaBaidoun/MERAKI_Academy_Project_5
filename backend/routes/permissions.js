7174bdc924acd96afd8f7e842e0391d64abf4498
NadiaBaidoun <nadia.w.baidoun@gmail.com>
2022-06-01 15:12:45 +0300

    permissions

--- backend/routes/permissions.js
+++ backend/routes/permissions.js
@@ -2,7 +2,12 @@ const express = require("express");
 
 
 
+const { createPermission } = require("../controllers/permission");
+
 const permissionRouter = express.Router();
+
+
+permissionRouter.post("/:role_id", createPermission);
 
 
 
