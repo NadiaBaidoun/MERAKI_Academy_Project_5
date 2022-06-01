d0afa57019466a2aef4d775545a381724bc67102
NadiaBaidoun <nadia.w.baidoun@gmail.com>
2022-06-01 21:33:20 +0300

    create post

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,5 +1,18 @@
 const express = require("express");
 
+//controllers
+const { createPost } = require("../controllers/posts");
+
 const postsRouter = express.Router()
+
+//Middlewares
+const authentication = require("../middlewares/authentication");
+
+postsRouter.post(
+    "/",
+    authentication,
+    createPost,
+  );
+
 
 module.exports = postsRouter;
