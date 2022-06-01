71949b2289b2fb8e0c2bd8b3e8f283541094385d
NadiaBaidoun <nadia.w.baidoun@gmail.com>
2022-06-01 14:53:34 +0300

    create role

--- backend/routes/role.js
+++ backend/routes/role.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const { createNewRole } = require("../controllers/role");
 
 
 
 const roleRouter = express.Router();
 
-
+roleRouter.post("/",createNewRole);
 
 module.exports = roleRouter;
