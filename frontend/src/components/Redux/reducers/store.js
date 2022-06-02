920134be309e30eacc84685fd91d3f9d14b88ed2
NadiaBaidoun <nadia.w.baidoun@gmail.com>
2022-06-02 23:12:01 +0300

    new Post

--- frontend/src/components/Redux/reducers/store.js
+++ frontend/src/components/Redux/reducers/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 //reducers
-
+import postReducer from './Post'
 export default configureStore({
-  reducer: {},
+  reducer: {
+    posts:postReducer,
+  },
 });
