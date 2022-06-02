c13d278cff46567230a6dfd1e8e21d0d7105eef6
FarahQandalaft <qandalaft.f@gmail.com>
2022-06-03 00:27:47 +0300

    register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import Register from "./components/Register";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<h1>WELCOME TO FACEBOOK CLONE</h1>} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   );
