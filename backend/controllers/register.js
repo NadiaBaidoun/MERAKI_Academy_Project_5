1c9d89a98e2c991fe2cb1ee95e903a0afe4c1253
NadiaBaidoun <nadia.w.baidoun@gmail.com>
2022-06-07 21:48:37 +0300

    edit register func

--- backend/controllers/register.js
+++ backend/controllers/register.js
@@ -3,23 +3,19 @@ const connection = require("../models/db");
 const bcrypt = require("bcrypt");
 
 const register = async (req, res) => {
-  const {
-    firstName,
-    lastName,
-    age,
-    country,
-    email,
-    image,
-    bio,
-    password,
-    role_id,
-  } = req.body;
+  const { firstName, lastName, age, country, email, password, role_id } =
+    req.body;
+
+  const image =
+    "https://www.icmetl.org/wp-content/uploads/2020/11/user-icon-human-person-sign-vector-10206693.png";
+  const cover =
+    "https://media.istockphoto.com/photos/empty-white-studio-room-abstract-background-picture-id1147521090?b=1&k=20&m=1147521090&s=170667a&w=0&h=z2Syz9Pwcb55xKIIbp08AFPJyVjZM28t5IZLyyhqV3k=";
 
   const salt = 10;
 
   const encryptedPassword = await bcrypt.hash(password, salt);
   const userName = `${firstName} ${lastName}`;
-  const query = `INSERT INTO users (firstName, lastName,userName, birthdate, country,image,email,bio, password, role_id) VALUES (?,?,?,?,?,?,?,?,?,?)`;
+  const query = `INSERT INTO users (firstName, lastName,userName, birthdate, country,image,email,cover, password, role_id) VALUES (?,?,?,?,?,?,?,?,?,?)`;
   const data = [
     firstName,
     lastName,
@@ -28,7 +24,7 @@ const register = async (req, res) => {
     country,
     image,
     email,
-    bio,
+    cover,
     encryptedPassword,
     role_id,
   ];
