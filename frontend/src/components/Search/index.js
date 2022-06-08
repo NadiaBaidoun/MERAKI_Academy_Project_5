9f35ea15632c5bb94acb666fd8ba9782eeb4c836
FaresAlSayyedAhmad <fares.a.alsayyed@gmail.com>
2022-06-08 13:55:30 +0300

    edit the searchcomponent and the searchresult

--- frontend/src/components/Search/index.js
+++ frontend/src/components/Search/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 import axios from "axios";
 
@@ -7,8 +7,16 @@ import { setUserName } from "../Redux/reducers/users";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
+
+
 const Search = () => {
   const [name, setName] = useState("");
+
+  const { token } = useSelector((state) => {
+    return {
+      token: state.auth.token,
+    };
+  });
 
   //=================================
   const dispatch = useDispatch();
@@ -24,11 +32,9 @@ const Search = () => {
         console.log(result);
         if (result.data.success) {
           dispatch(setUserName(result.data.result));
-          setShow(true);
         }
       })
       .catch((error) => {
-        setShow(false);
         console.log(error.response.data.message);
       });
   };
