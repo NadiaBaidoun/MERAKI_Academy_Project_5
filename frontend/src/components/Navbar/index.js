34bb1103acc5e08e3da51113e4803b1fdb5447f9
FarahQandalaft <qandalaft.f@gmail.com>
2022-06-08 22:04:20 +0300

    handling navBar

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./style.css";
 
 import { useDispatch, useSelector } from "react-redux";
 
 import { logout } from "../Redux/reducers/auth";
 import Search from "../Search";
-
+import { FaFacebook } from "react-icons/fa";
 const Navbar = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const { isLoggedIn } = useSelector((state) => {
     return {
@@ -24,8 +25,18 @@ const Navbar = () => {
     <div className="navbar-container">
       {isLoggedIn ? (
         <div className="navbar">
-          <div>
-            <Search />
+          <div className="left-navbar">
+            <div
+              className="facebook-logo"
+              onClick={() => {
+                navigate("/home");
+              }}
+            >
+              <FaFacebook className="logo" />
+            </div>
+            <div className="search-container">
+              <Search />
+            </div>
           </div>
           <div className="Links-container">
             <Link className="link" to={"/home"}>
