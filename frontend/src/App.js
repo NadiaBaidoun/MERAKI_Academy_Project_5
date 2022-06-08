df890d2e17c3ff95540bdc07b76f31d4f20e96c3
FaresAlSayyedAhmad <fares.a.alsayyed@gmail.com>
2022-06-08 13:30:14 +0300

    add search and searchresult routes and components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import Navbar from "./components/Navbar";
 import Dashboard from "./components/Dashboard";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
+import Search from "./components/Search";
+import SearchResult from "./components/SearchResult";
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/search/" element={<Search />} />
+        <Route path="/search/users" element={<SearchResult />} />
 
         <Route path="/home" element={<Dashboard />} />
         <Route path="/profile" element={<Profile />} />
